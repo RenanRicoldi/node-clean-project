e6a97725f8a7f1498abbe3b0c9042f16c256064e
Renan Ricoldi <renan.ricoldi.frois@gmail.com>
2025-03-03 23:36:09 -0300

    feat: add express http api

---
+++ src/infra/http/express/htpp-api.express.ts
@@ -0,0 +1,47 @@
+import { IHttpApi } from '../interfaces/http-api.interface'
+import express, { Express, Request, Response } from 'express'
+import { IHttpRoute } from '../interfaces/http-route.interface'
+import { HttpResponse } from '../interfaces/http-response.interface'
+import { HttpRequest } from '../interfaces/http-request.interface'
+
+export class ApiExpress implements IHttpApi {
+  private app: Express
+
+  private constructor(routes: IHttpRoute[]) {
+    this.app = express()
+    this.setupRoutes(routes)
+  }
+
+  static create(routes: IHttpRoute[]): ApiExpress {
+    return new ApiExpress(routes)
+  }
+
+  public start(port: number): void {
+    this.app.listen(port, () => {
+      console.log(`Server is running on port ${port}`)
+    })
+  }
+
+  private setupRoutes(routes: IHttpRoute[]): void {
+    routes.forEach((route) => {
+      this.app[route.method](route.path, this.adaptRoute(route.handler))
+    })
+  }
+
+  private adaptRoute(
+    handler: (httpRequest: HttpRequest) => HttpResponse | Promise<HttpResponse>,
+  ) {
+    return async (req: Request, res: Response) => {
+      const httpRequest: HttpRequest = {
+        body: req.body,
+        params: req.params,
+        query: req.query as Record<string, string | string[]>,
+        headers: req.headers as Record<string, string>,
+      }
+
+      const httpResponse = await handler(httpRequest)
+
+      res.status(httpResponse.status).json(httpResponse.body)
+    }
+  }
+}
